4cb1ad0da2b512320f144dfbd68c3b3185deb049
GanaOD <ganeshaodonnell@pm.me>
2025-02-11 15:15:22 +0100

    Fix bug in len calculation (wasnt a proper conversion to index notation)
    Fix bug: change space_written flag back to 0 when back in spaces
    
    In process of fixing this, using printf debug statements: terminal
    output appeared in different order than calls (a printf debug statement
    appeared after write call, final output: understand now that the \n in
    printf statements also forces a buffer flush)
    
    WIP: space handling in specific case: moving from 1st word handling
    (storing) into 1st word of str that gets written: no leading space!

--- Level_04/rostring/rostring.c
+++ Level_04/rostring/rostring.c
@@ -1,28 +1,83 @@
 #include <unistd.h> // for write
+#include <stdlib.h> // malloc
+#include <stdio.h> // printf debug statements
 
-int in_word(char c)
+int is_space(char c)
 {
-  if (c == ' ' || c >= 9 && c <= 13)
-    return (0);
-  else 
-    return (1);
+  return (c == ' ' || (c >= 9 && c <= 13));
 }
 
-while (*str)
+char  *store_word(char *str, int len)
 {
-    // Handle initial spaces
-    if (!in_word(*str) && in_word_flag == 0)
-        str++;
-    // Find & store first word
-    else if (in_word(*str) && in_word_flag == 0)
+  char  *word = malloc(len + 1);
+  int   i = 0; // index for copying
+
+  while (i < len)
+  {
+    word[i] = str[i];
+    i++;
+  }
+  word[i] = '\0';
+
+  printf("Debug: word to be stored: %s\n", word);
+
+  return (word);
+}
+
+void  rostring(char *str)
+{
+  char  *first_word;
+  int   len = 0;
+  int   space_written = 0;
+
+  // handle leading space
+  while (is_space(*str))
+    str++;
+  
+  printf("Debug: Handled leading space\n");
+
+  while (str[len] && !is_space(str[len]))
+    len++;
+  if (!len)
+    return;
+  printf("Debug: len of first word = %d\n", len);
+
+  first_word = store_word(str, len);
+  
+  // move str pointer to 1 after first word to continue overall incrementation
+  str += len;
+  
+  // Handle rest of string
+  while (*str)
+  {
+    // 2 conditions
+    if (is_space(*str))
     {
-        // First word logic...
-        
-        // Then immediately try to handle rest of string?
-        if (!in_word(*str) && in_word_flag == 1)
-        {
-            // Next word logic...
-        }
+      while (is_space(*str))
+        str++; // increment through all spaces
+      space_written = 0; // reset flag
+      if (*str) // if not end of string
+        write (1, " ", 1); // write 1 space 
     }
+    else
+    {
+      write (1, str++, 1);
+      space_written = 1;
+    }
+  }
+  // space between last word processed in str and the final word (stored)
+  if (space_written)
+    write (1, " ", 1);
+
+  write (1, first_word, len);
+  free (first_word);
 }
 
+
+int main(int ac, char **av)
+{
+  if (ac > 1)
+    rostring(av[1]);
+  write (1, "\n", 1);
+  return (0);
+}
