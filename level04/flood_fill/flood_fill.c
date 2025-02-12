d7292d627bd7d5a3a70accbd24b492fc57bd5d17
GanaOD <ganeshaodonnell@pm.me>
2025-02-12 18:52:12 +0100

    list traversal
    passing function as param

--- level04/flood_fill/flood_fill.c
+++ level04/flood_fill/flood_fill.c
@@ -7,7 +7,9 @@ void	fill(char **tab, t_point size, char original_char, int curr_row, int curr_c
 	if (curr_row < 0 || curr_col < 0 || curr_row >= size.y || curr_col >= size.x)
 		return ;
 
-	// Base case 2: return if current cell already filled ('F') or doesn't match original char
+	// Base case 2: return if: 
+	// current cell already filled ('F') 
+	// or doesn't match original char
 	// Prevents infinite recursion & ensures only filling connected region
 	if (tab[curr_row][curr_col] == 'F' || tab[curr_row][curr_col] != original_char)
 		return ;
@@ -18,9 +20,9 @@ void	fill(char **tab, t_point size, char original_char, int curr_row, int curr_c
 	// Recursively fill all adjacent cells: DFS
 	// Creates a "flood" effect, spreading to all connected matching cells
 	fill(tab, size, original_char, curr_row - 1, curr_col);  // Up
-    fill(tab, size, original_char, curr_row + 1, curr_col);  // Down
-    fill(tab, size, original_char, curr_row, curr_col - 1);  // Left
-    fill(tab, size, original_char, curr_row, curr_col + 1);  // Right
+	fill(tab, size, original_char, curr_row + 1, curr_col);  // Down
+	fill(tab, size, original_char, curr_row, curr_col - 1);  // Left
+	fill(tab, size, original_char, curr_row, curr_col + 1);  // Right
 }
 
 // Entry point function, initiates flood fill operation
--- level04/ft_list_foreach/ft_list_foreach.c
+++ level04/ft_list_foreach/ft_list_foreach.c
@@ -1,15 +1,15 @@
 typedef	struct s_list
 {
 	struct s_list	*next;
-	void			*data;
+	void					*data;
 }	t_list;
 
 
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
-	while (begin_list)
+	while (begin_list) // while ptr to struct is a valid ptr
 	{
-		f(begin_list->data);
-		begin_list = begin_list->next;
+		f(begin_list->data); // apply function to each node's data
+		begin_list = begin_list->next; // "moves" begin_list ptr to next node in list
 	}
 }
